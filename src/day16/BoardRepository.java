911fa2299f196e3cb3d9614aff5c30434703963c
guguping <qkfhandur@naver.com>
2023-03-15 12:47:46 +0900

    오전_3

--- src/day16/BoardRepository.java
+++ src/day16/BoardRepository.java
@@ -1,5 +1,61 @@
 package day16;
 
-public class BoardRepository {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import day15.StudentDTO;
+
+public class BoardRepository {
+	//데이터 베이스에 가공 정보를 저장하는곳 데이터 베이스에서 가져오고 저장하기만 가능하다
+		private List<BoardDTO> list = new ArrayList<>();
+		private Map<String, BoardDTO> bRepository = new HashMap<>();
+
+		public void save(BoardDTO boardDTO) {
+			bRepository.put(boardDTO.getBno(), boardDTO);
+			// add는 list의 숨겨진 메소드이다
+			// 저장되면 true를 리턴하고 저장 실패하면 false를 리턴한다
+		}
+		
+		public Map<String, BoardDTO> findAll(){
+			return bRepository;
+		}
+		
+		public BoardDTO findById(String bno){
+			for(BoardDTO b : list) {
+				if(b.getBno().equals(bno)) {
+					return b;
+				}
+			}
+			return null;
+		}
+		
+		public boolean update(BoardDTO boardDTO , String bno) {
+			for(BoardDTO b : list) {
+				if(b.getBno().equals(bno)) {
+					b.setTitle(boardDTO.getTitle());
+					b.setWriter(boardDTO.getWriter());
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		// 내가만든 삭제
+		public void remove(BoardDTO boardDTO) {
+			
+			list.remove(boardDTO);
+		}
+		// 선생님이 만든 삭제
+		public boolean delete(String bno) {
+			for(BoardDTO b : list) {
+				if(b.getBno().equals(bno)) {
+					
+					list.remove(b);
+					return true;
+				}
+			}
+			return false;
+		}
 }
--- src/day16/BoardService.java
+++ src/day16/BoardService.java
@@ -1,5 +1,88 @@
 package day16;
 
-public class BoardService {
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+//담은 정보를 가공하는 곳
 
+public class BoardService {
+	BoardRepository br = new BoardRepository();
+	Scanner sc = new Scanner(System.in);
+
+	public void save() {
+		BoardDTO boardDTO = new BoardDTO();
+		System.out.print("제목 >");
+		boardDTO.setTitle(sc.nextLine());
+		System.out.print("작성자 >");
+		boardDTO.setWriter(sc.next());
+		sc.nextLine();
+		br.save(boardDTO);
+	}
+
+	public void findAll() {
+		Map<String, BoardDTO> list = br.findAll();
+		System.out.println("글번호\t제목\t\t작성자\t조회수\t게시일");
+		System.out.println("==============================================");
+		for(String u : list.keySet()) {
+			list.get(u).print();
+		}
+	}
+
+	public void findById() {
+		System.out.println("읽을 글 번호");
+		String bno = sc.next();
+		BoardDTO boardDTO = br.findById(bno);
+		if (boardDTO == null) {
+			System.out.println("찾을 수 없는 글입니다");
+		} else {
+			boardDTO.increasesetCnt();
+			System.out.println("글번호\t제목\t\t작성자\t조회수\t게시일");
+			System.out.println("==============================================");
+			boardDTO.print();
+		}
+	}
+
+	public void update() {
+		System.out.print("수정 글 번호>");
+		String bno = sc.nextLine();
+		BoardDTO b = br.findById(bno);
+		if (b == null) {
+			System.out.println("찾을 수 없는 글입니다");
+		} else {
+			BoardDTO boardDTO = new BoardDTO();
+			System.out.print("수정할 제목 >");
+			boardDTO.setTitle(sc.nextLine());
+			System.out.print("수정할 작성자 >");
+			boardDTO.setWriter(sc.nextLine());
+			if (br.update(boardDTO, bno)) {
+				System.out.println("업데이트 완료!");
+			} else {
+				System.out.println("업데이트 실패!");
+			}
+		}
+	}
+	
+	// 내가 만든 삭제
+	public void remove() {
+		System.out.println("삭제할 글 번호 >");
+		String bno = sc.next();sc.nextLine();
+		BoardDTO boardDTO = br.findById(bno);
+		if (boardDTO == null) {
+			System.out.println("찾을 수 없는 글입니다");
+		} else {
+			br.remove(boardDTO);
+			System.out.println("삭제 완료");
+		}
+	}
+	
+	// 선생님이 만든 삭제
+	public void delete() {
+		System.out.println("삭제할 글 번호");
+		String bno = sc.next();
+		if(br.delete(bno)) {
+			System.out.println("삭제 완료");
+		}else {
+			System.out.println("삭제 실패");
+		}
+	}
 }
